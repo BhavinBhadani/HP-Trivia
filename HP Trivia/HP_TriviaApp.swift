dc923915f9f3fbdd1e0a03e09cf03941ecd1067c
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-02 09:19:12 +0530

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Bhavin Bhadani on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
