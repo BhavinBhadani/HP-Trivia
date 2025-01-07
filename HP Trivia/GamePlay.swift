08a6d888c3f23f060376914d3354ea9e72aefdb9
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-07 10:36:16 +0530

    celebration views

--- HP Trivia/GamePlay.swift
+++ HP Trivia/GamePlay.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct GamePlay: View {
     @Environment(\.dismiss) private var dismiss
     @State private var animateViewsIn = false
-    
+    @State private var tappedCorrectAnswer = false
+
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -110,12 +111,75 @@ struct GamePlay: View {
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
                 .foregroundStyle(.white)
+                
+                VStack {
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("5")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .transition(.offset(y: -geo.size.height/4))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(2), value: tappedCorrectAnswer)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Text("Brilliant!")
+                                .font(.custom(Constants.hpFont, size: 100))
+                                .transition(.scale.combined(with: .offset(y: -geo.size.height/2)))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 1).delay(1), value: tappedCorrectAnswer)
+
+                    Spacer()
+                    
+                    Text("Answer 1")
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .padding(10)
+                        .frame(width: geo.size.width/2.15, height: 80)
+                        .background(.green.opacity(0.5))
+                        .clipShape(.rect(cornerRadius: 25))
+                        .scaleEffect(2)
+                    
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+                    
+                    VStack {
+                        if tappedCorrectAnswer {
+                            Button("Next Level>") {
+                                
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.blue.opacity(0.5))
+                            .font(.largeTitle)
+                            .transition(.offset(y: geo.size.height/3))
+                        }
+                    }
+                    .animation(.easeInOut(duration: 2.7).delay(2.7), value: tappedCorrectAnswer)
+
+                    Group {
+                        Spacer()
+                        Spacer()
+                    }
+                }
+                .foregroundStyle(.white)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
         .onAppear {
-            animateViewsIn = true
+            //animateViewsIn = true
         }
     }
 }
